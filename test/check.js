5ec2d00d2b9b5436154813711938cf45c6791a3d
Victoria-Vladimirova <wiktorija.wladimirowa@gmail.com>
2015-11-19 00:47:09 +0500

    Исправлены замечания

--- test/check.js
+++ test/check.js
@@ -13,6 +13,8 @@ describe('Check', function () {
 
     if ({}.check) {
         describe('Extra credit', function () {
+
+            // проверки прямых условий
             it('should check that target hasKeys', function () {
                 assert.ok(person.check.hasKeys(['name', 'age']));
             });
@@ -39,6 +41,54 @@ describe('Check', function () {
 
             it('should check that target hasWordsCount', function () {
                 assert.ok(str.check.hasWordsCount(2));
+            });
+
+            // проверки отрицательных условий
+            it('should check that target not hasKeys', function () {
+                assert.ok(person.check.not.hasKeys(['name', 'birthDate']));
+            });
+
+            it('should check that target not hasValueType', function () {
+                assert.ok(person.check.not.hasValueType('name', Number));
+            });
+
+            it('should check that target not hasKeys', function () {
+                assert.ok(numbers.check.not.hasKeys(['0', '3', '2']));
+            });
+
+            it('should check that target not hasLength', function () {
+                assert.ok(numbers.check.not.hasLength(4));
+            });
+
+            it('should check that target not containsValues', function () {
+                assert.ok(numbers.check.not.containsValues([2, 1, 37]));
+            });
+
+            it('should check that target not hasParamsCount', function () {
+                assert.ok(func.check.not.hasParamsCount(3));
+            });
+
+            it('should check that target not hasWordsCount', function () {
+                assert.ok(str.check.not.hasWordsCount(3));
+            });
+
+            // проверки обёртки
+            it('should check that wrapped target hasKeys', function () {
+                assert.ok(check.wrap(person).check.hasKeys(['name', 'age']));
+            });
+
+            it('should check that wrapped target not hasKeys', function () {
+                assert.ok(check.wrap(person).check.not.hasKeys(['name', 'birthDate']));
+            });
+
+            it('should check that wrapped null isNull', function () {
+                assert.ok(check.wrap(null).isNull());
+            });
+
+
+            // проверка того, что методы не передаются по иерархии наследования
+            it('should check that inherited check attribute is undefined', function () {
+                assert.ok(new Date().check === undefined);
             });
         });
     } else {
